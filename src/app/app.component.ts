2415856d8e9db920f4ad7791b9f0b9d1b795241f
Tatiana <tats.krauchanka@gmail.com>
2025-03-29 21:56:22 +0300

    feat: add filter and error handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,31 @@ import { CardComponent } from './components/card/card.component';
 import { ICard } from './utils/interfaces';
 import { CommonModule, NgFor } from '@angular/common';
 import { CardService } from './services/card.service';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { ErrorComponent } from './components/error/error.component';
+import { ErrorService } from './services/error.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CardComponent, NgFor, CommonModule],
+  imports: [CardComponent, NgFor, CommonModule, FormsModule, ErrorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   cards$: Observable<ICard[]>;
+  name = '';
 
-  constructor(private cardService: CardService) {
+  constructor(private cardService: CardService, private errorService: ErrorService) {
     this.cards$ = this.cardService.getAllCards();
+  }
+
+  updateCards() {
+    this.cards$ = this.cardService.getCharacter(this.name.toLowerCase());
+
+    if (!this.name) {
+      this.errorService.clear();
+    }
   }
 }
---
+++ src/app/components/error/error.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ErrorService } from '../../services/error.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-error',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './error.component.html',
+  styleUrl: './error.component.scss'
+})
+export class ErrorComponent {
+  constructor(public errorService: ErrorService) { }
+}
