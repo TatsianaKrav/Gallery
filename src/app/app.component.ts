7407e4c820af97b481bc6d2b758384d55a4062aa
Tatiana <tats.krauchanka@gmail.com>
2025-04-01 10:02:05 +0300

    feat: remove for pr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { CardComponent } from './components/card/card.component';
-import { ICard } from './utils/interfaces';
-import { CommonModule, NgFor } from '@angular/common';
-import { CardService } from './services/card.service';
-import { map, Observable } from 'rxjs';
-import { FormsModule } from '@angular/forms';
-import { ErrorComponent } from './components/error/error.component';
-import { ErrorService } from './services/error.service';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [CardComponent, NgFor, CommonModule, ErrorComponent, FormsModule],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  cards$: Observable<ICard[]>;
-  name = '';
 
-  constructor(private cardService: CardService, private errorService: ErrorService) {
-    this.cards$ = this.cardService.getAllCards();
-  }
-
-  updateCards() {
-    this.cards$ = this.cardService.getCharacter(this.name.toLowerCase());
-
-    if (!this.name) {
-      this.errorService.clear();
-    }
-  }
 }
